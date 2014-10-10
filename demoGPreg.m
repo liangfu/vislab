b88465be76d0fa55eafceaf0d1ac7a140686ca7f
Liangfu Chen <liangfu.chen@nlpr.ia.ac.cn>
2014-10-10 12:28:37 +0800

    add scatteredInterp function for 2d interpolation;

--- demoGPreg.m
+++ demoGPreg.m
@@ -22,7 +22,30 @@ xtest=[-4:0.1:4];
 [meantest,vartest,logpygx]=GPreg(xtrain,ytrain,xtest,par,covfn);
 
 figure
-plot(xtest,meantest,'r-');  hold on; % mean prediction
-plot(xtest,meantest-sqrt(vartest),'g-'); % standard error bars (on predicting clean output)
-plot(xtest,meantest+sqrt(vartest),'g-');
-plot(xtrain,ytrain,'.');
+subplot(221),plot(xtest,meantest,'r-');  title('mean prediction');
+subplot(222),plot(xtest,meantest-sqrt(vartest),'g-'); title('standard error bars')
+subplot(223),plot(xtest,meantest+sqrt(vartest),'g-');
+subplot(224),plot(xtrain,ytrain,'.');
+
+function K = covfnGE(x,y,par)
+% COVFNGE Gamma Exponential Covariance Function
+%K = covfnGE(x,y,par)
+K = par(1).value.*exp(-par(2).value*(sqdist(x,y)).^(0.5*par(3).value));
+if length(par)==4
+    K = K + par(4).value*eye(size(K,1));
+end
+
+function l=logdet(A)
+% LOGDET Log determinant of a positive definite matrix computed in a
+%numerically more stable manner
+[u s v]=svd(A); 
+l=sum(log(diag(s)+1.0e-20));
+
+function D = sqdist(x,y)
+% SQDIST Square distance between vectors in x and y
+% D = sqdist(x,y)
+[d m]=size(x); n=size(y,2);
+xx=repmat(sum(x.^2,1),n,1); 
+yy=repmat(sum(y.^2,1),m,1); 
+xy=x'*y; 
+D = xx'+yy-2*xy;
--- gptest.m
+++ gptest.m
@@ -15,3 +15,26 @@ function kappa=rbf(p, q, phi)
   tmp = repmat(p',size(q))-repmat(q, size(p'));
   invbeta = phi(4)^-1;
   kappa = phi(1).*exp(-0.5*phi(2)*tmp.^2)+phi(3)+invbeta*eye(size(p,1));
+
+  function K = covfnGE(x,y,par)
+% COVFNGE Gamma Exponential Covariance Function
+%K = covfnGE(x,y,par)
+K = par(1).value.*exp(-par(2).value*(sqdist(x,y)).^(0.5*par(3).value));
+if length(par)==4
+    K = K + par(4).value*eye(size(K,1));
+end
+
+function l=logdet(A)
+% LOGDET Log determinant of a positive definite matrix computed in a
+%numerically more stable manner
+[u s v]=svd(A); 
+l=sum(log(diag(s)+1.0e-20));
+
+function D = sqdist(x,y)
+% SQDIST Square distance between vectors in x and y
+% D = sqdist(x,y)
+[d m]=size(x); n=size(y,2);
+xx=repmat(sum(x.^2,1),n,1); 
+yy=repmat(sum(y.^2,1),m,1); 
+xy=x'*y; 
+D = xx'+yy-2*xy;
