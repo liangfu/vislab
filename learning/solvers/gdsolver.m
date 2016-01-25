c2cd8e6923656266842fd986e9b66f6906eae75d
Liangfu Chen <liangfu.chen@nlpr.ia.ac.cn>
2016-01-25 16:07:31 +0800

    add initialization parameter;

--- learning/solvers/gdsolver.m
+++ learning/solvers/gdsolver.m
@@ -1,4 +1,4 @@
-function [xi,yi]=gdsolver(fx,dfx)
+function [xi,yi]=gdsolver(fx,dfx,x0)
 % GDSOLVER demostrates gradient descent algorithm on Wikipedia
     
 if nargin<1
@@ -8,24 +8,21 @@ if nargin<1
   dfx{1}=@(x) [3,sin(x(2)*x(3))*x(3),sin(x(2)*x(3))*x(2)];
   dfx{2}=@(x) [8*x(1),-1250*x(2)+2,0];
   dfx{3}=@(x) [-x(2)*exp(-x(1)*x(2)),-x(1)*exp(-x(1)*x(2)),20];
+  x0=zeros([3,1]);
 end
 
-Gx=@(x,fx) (cellfun(@(c) c(x),fx))';      % nonlinear system
-% eval by calling `Gx(x0,fx)`
-Fx=@(x,Gx,fx) .5*Gx(x,fx).*Gx(x,fx);      % objective function
-% eval by calling `Fx(x0,Gx,fx)`
-J_G=@(x,dfx) cell2mat(cellfun(@(c) (c(x))',dfx,'uniformoutput',false));
-% eval by calling `J_G(x0,dfx)`
-dFx=@(x,J_G,dfx,Gx,fx) (J_G(x,dfx))'*Gx(x,fx); % derivative
-% eval by calling `dFx(x0,J_G,dfx,Gx,fx)`
+Gx=@(x,fx) (cellfun(@(c) c(x),fx))';      % `Gx(x0,fx)` - nonlinear system
+Fx=@(x,Gx,fx) .5*Gx(x,fx).*Gx(x,fx);      % `Fx(x0,Gx,fx)` - objective function
+J_G=@(x,dfx) cell2mat(cellfun(@(c) (c(x))',dfx,'uniformoutput',false)); % `J_G(x0,dfx)`
+dFx=@(x,J_G,dfx,Gx,fx) (J_G(x,dfx))'*Gx(x,fx); % `dFx(x0,J_G,dfx,Gx,fx)` - derivative
 
 % parameters
 gamma=1e-3;
-x0=[0;0;0];
 epsilon=0.1;
 maxiter=1000;
 
 % initialize
+if ~exist('x0','var'),x0=zeros([length(fx),1]);end
 xi=x0;
 yi=sum(Fx(xi,Gx,fx)); % eval objective
 iter=2;
